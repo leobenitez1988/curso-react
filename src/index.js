69c18916dc6a98ee23a265c68a70d01a61210e9e
Leo Benítez <l.benitez@preving.com>
2021-02-18 21:23:04 +0100

    primer componente

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Card from './components/Card'
 
-const user = {
-  firstName: 'Leo',
-  lastName: 'Benítez',
-  avatar: 'https://i.pinimg.com/originals/e9/57/2a/e9572a70726980ed5445c02e1058760b.png'
-};
-
-function getName (user){
-  if(user){
-    return `${user.firstName} ${user.lastName}`
-  }
-  return 'Stranger';
-};
-
-const element = (
-  <div>
-    <h1>{getName(user)}</h1>
-    <img src={user.avatar}></img>
-  </div>
-);
+const element = <Card></Card>
 
 const container = document.getElementById('root')
-
 ReactDOM.render(element, container)
